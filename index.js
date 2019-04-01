5e6fb86b900c4894a8e8516eaaeeb89e6155541a
Szymon Frankowski <sz.frankyy@gmail.com>
2019-04-01 11:42:17 +0200

    missing listFiles

--- index.js
+++ index.js
@@ -4,107 +4,96 @@ const path = require('path');
 
 
 
-
-
-
-
-
+const filePath = "./data/file.txt";
+const params = {
+  Bucket: 'lcloud-427-ag',
+  Body: fs.createReadStream(filePath),
+  Key: "folder/" + Date.now() + "_" + path.basename(filePath)
+};
 const s3 = new AWS.S3({
   region: 'eu-central-1',
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-async function listAllObjectsFromS3Bucket(bucket, prefix, filter = /.*/,command=(item)=>{}) {
+
+
+
+async function listAllObjectsFromS3Bucket(bucket, prefix, filter = /.*/, command = (item) => {}) {
   let isTruncated = true;
   let marker;
-  while(isTruncated) {
-    let params = { Bucket: bucket };
-    if (prefix) params.Prefix = prefix;
-    if (marker) params.Marker = marker;
+
+  
+  while (isTruncated) {
+
+    let params = {
+      Bucket: bucket
+    };
+
+    if (prefix) {
+      params.Prefix = prefix;
+    };
+
+    if (marker) {
+      params.Marker = marker;
+    };
+
     try {
+
       const response = await s3.listObjects(params).promise();
+
       response.Contents.forEach(item => {
+        console.log(item.Key);
         !item.Key.match(filter) || ((console.log(item.Key) && result.push(item.Key)));
         command(item.Key);
       });
+
       isTruncated = response.IsTruncated;
+
       if (isTruncated) {
         marker = response.Contents.slice(-1)[0].Key;
       }
-  } catch(error) {
+
+    } catch (error) {
       throw error;
     }
   }
-
-
 }
 
 
 
-
-
-
-const filePath = "./data/file.txt";
-
-//configuring parameters
-var params = {
-  Bucket: 'lcloud-427-ag',
-  Body : fs.createReadStream(filePath),
-  Key : "folder/"+Date.now()+"_"+path.basename(filePath)
-};
-
-
-s3.upload(params, function (err, data) {
-  //handle error
+s3.upload(params, (err, data) => {
   if (err) {
     console.log("Error", err);
+    return;
   }
-
-  //success
   if (data) {
     console.log("Uploaded in:", data.Location);
   }
 });
 
 
-//listAllObjectsFromS3Bucket('lcloud-427-ag',null);
+
+listAllObjectsFromS3Bucket('lcloud-427-ag',null);
 
 
+listAllObjectsFromS3Bucket('lcloud-427-ag', null, /11/, deleteFile);
 
-
-
-
-listAllObjectsFromS3Bucket('lcloud-427-ag',null,/410756/,deleteFile);
-
-function deleteFile(item){
+function deleteFile(item) {
   const params = {
-  Bucket: 'lcloud-427-ag',
-  Key: item
+    Bucket: 'lcloud-427-ag',
+    Key: item
+  };
 
-};
+  s3.deleteObject(params, (err, data) => {
+    if (err) {
+      console.log("Error", err);
+      return;
+    }
 
-s3.deleteObject(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
-    //console.log(toRemove);
+    if (data) {
+      console.log(data);
+    }
+  });
+
 }
-
-
-//process.exit();
-
-// const deleteParam = {
-//     Bucket: 'lcloud-427-ag',
-//     Delete: {
-//         Objects: [
-//             {Key: 'a.txt'},
-//             {Key: 'b.txt'},
-//             {Key: 'c.txt'}
-//         ]
-//     }
-// };
-// s3.deleteObjects(deleteParam, function(err, data) {
-//     if (err) console.log(err, err.stack);
-//     else console.log('delete', data);
-// });
